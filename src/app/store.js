a2da10358e957ba352ec94e8fbcfc733ae9fe7b9
SNEHANSHU-CODE <snehanshusekhar99@gmail.com>
2025-05-24 19:56:14 +0530

    Persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from "../pages/slice/cartSlice";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Uses localStorage
+import { combineReducers } from 'redux';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+// Combine reducers (in case you add more in the future)
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
+
+// Persist configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// Wrap root reducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store with middleware to handle non-serializable actions
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/FLUSH', 'persist/PAUSE', 'persist/PURGE', 'persist/REGISTER'],
+      },
+    }),
+});
+
+// Export the persistor
+export const persistor = persistStore(store);
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
-// Import Bootstrap CSS & JS
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
